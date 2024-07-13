14b77210bd6e898151956c8d385bb187422e24c2
RafaelMourao <mourao.ea@gmail.com>
2024-07-13 14:28:15 -0300

    feat: add hold shift to place multiple

--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -1,12 +1,85 @@
 // src/components/Toolbox.tsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addElement } from "../store/circuitSlice";
 import { v4 as uuidv4 } from "uuid";
-// import zIndex from "@material-ui/core/styles/zIndex";
+import { type CircuitElementProps } from "../store/circuitSlice";
 
 const Toolbox: React.FC = () => {
   const dispatch = useDispatch();
+  const [elementToPlace, setElementToPlace] = useState<CircuitElementProps | null>(null);
+  const [isPlacing, setIsPlacing] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [rotation, setRotation] = useState(0);
+  const [shift, setShift] = useState(false);
+  const [gridSize] = useState(32);
+
+  useEffect(() => {
+    const handleMouseUp = (event: MouseEvent) => {
+      if (isPlacing && elementToPlace) {
+        const newElement = {
+          ...elementToPlace,
+          position: { x: Math.round(event.clientX / gridSize) * gridSize, y: Math.round(event.clientY / gridSize) * gridSize },
+          rotation,
+          id: uuidv4(),
+        };
+
+        dispatch(addElement(newElement));
+        if (!shift) {
+          setIsPlacing(false);
+          setRotation(0);
+          setElementToPlace(null);
+        }
+      }
+    };
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "q") {
+        setIsPlacing(false); // Stop placing when 'q' is pressed
+        setRotation(0); // Reset rotation after placing
+        setElementToPlace(null); // Clear the element to be placed
+      } else if (event.key === "r" && isPlacing && elementToPlace) {
+        const newRotation = (rotation + 1) % 4;
+        const newSize = [elementToPlace.size[1], elementToPlace.size[0]];
+        const newSpriteOffset = [elementToPlace.spriteOffsetRef[newRotation * 2], elementToPlace.spriteOffsetRef[newRotation * 2 + 1]];
+        const newBackgroundSize = [elementToPlace.backgroundSizeRef[newRotation * 2], elementToPlace.backgroundSizeRef[newRotation * 2 + 1]];
+
+        setElementToPlace({
+          ...elementToPlace,
+          rotation: newRotation,
+          size: newSize,
+          spriteOffset: newSpriteOffset,
+          backgroundSize: newBackgroundSize,
+        });
+        setRotation(newRotation);
+      } else if (event.key === "Shift") {
+        console.log("Shift key down");
+        setShift(true);
+      }
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (event.key === "Shift") {
+        console.log("Shift key up");
+        setShift(false);
+      }
+    };
+
+    const handleMouseMove = (event: MouseEvent) => {
+      setCursorPosition({ x: Math.round(event.clientX / gridSize) * gridSize, y: Math.round(event.clientY / gridSize) * gridSize });
+    };
+
+    window.addEventListener("mouseup", handleMouseUp);
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp);
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [isPlacing, elementToPlace, dispatch, rotation, gridSize, shift]);
 
   const handleAddElement = (element: { [key: string]: string | number[] }) => {
     const type = element.type as string;
@@ -19,7 +92,7 @@ const Toolbox: React.FC = () => {
     const backgroundSize = [element.backgroundSizeRef[0], element.backgroundSizeRef[1]] as number[];
 
     const newElement = {
-      id: uuidv4(),
+      id: "",
       type,
       size,
       sprite,
@@ -31,7 +104,22 @@ const Toolbox: React.FC = () => {
       position: { x: 0, y: 0 },
       rotation: 0,
     };
-    dispatch(addElement(newElement));
+
+    setElementToPlace(newElement); // Set the element to be placed on mouse click
+    setIsPlacing(true); // Set the placing flag to true
+  };
+
+  const getBackgroundImage = () => {
+    if (!elementToPlace) {
+      return {};
+    }
+    return {
+      backgroundImage: `url("${elementToPlace.sprite}")`,
+      backgroundPosition: `${elementToPlace.spriteOffset[0]}px ${elementToPlace.spriteOffset[1]}px`,
+      backgroundSize: `${elementToPlace.spriteSize[0]}px ${elementToPlace.spriteSize[1]}px`,
+      width: `${elementToPlace.backgroundSize[0]}px`,
+      height: `${elementToPlace.backgroundSize[1] + 10}px`,
+    };
   };
 
   return (
@@ -81,6 +169,21 @@ const Toolbox: React.FC = () => {
       >
         Constant Combinator
       </button>
+      {isPlacing && elementToPlace && (
+        <div
+          style={{
+            position: "fixed",
+            left: cursorPosition.x,
+            top: cursorPosition.y,
+            opacity: 0.5,
+            pointerEvents: "none",
+            transform: "translate(-50%, -50%)",
+            transformOrigin: "center",
+          }}
+        >
+          <img src={elementToPlace.sprite} alt={elementToPlace.type} style={{ ...getBackgroundImage() }} />
+        </div>
+      )}
     </div>
   );
 };
