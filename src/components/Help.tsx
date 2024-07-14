82c991ea63e6f9c0f55a6cca4eca0ddf1c1662f1
RafaelMourao <mourao.ea@gmail.com>
2024-07-14 11:38:33 -0300

    feat: add some content to help section

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -6,8 +6,20 @@ const Help: React.FC = () => {
       <div className="flex-space-between">
         <h2>Help</h2>
       </div>
-      <div className="panel-inset mb0 mt0"></div>
-      <div className="panel-inset-lighter mt0"></div>
+      <div className="panel-inset mb0 mt0">Shortcuts</div>
+      <div className="panel-inset-lighter mt0">
+        h: Help
+        <br />
+        e: Inventory
+        <br />
+        o: Debug
+        <br />
+        F1/h: Help
+        <br />
+        F2: Settings
+        <br />
+        Escape: Close drawers
+      </div>
     </div>
   );
 };
