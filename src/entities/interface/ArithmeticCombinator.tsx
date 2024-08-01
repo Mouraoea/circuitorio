b30388dc85d0e3d5827de08742cffccffb64d78e
RafaelMourao <mourao.ea@gmail.com>
2024-08-01 18:42:46 -0300

    feat: add combinators entities update when selecting a different operator

--- src/entities/interface/ArithmeticCombinator.tsx
+++ src/entities/interface/ArithmeticCombinator.tsx
@@ -3,11 +3,20 @@ import "../../App.css";
 import { EntityInterfaceProps } from "./EntityInterface";
 import { RenderSignalButton } from "../../utils/RenderSignalButton";
 import { useUIContext } from "../../context/UIContext";
+import { updateElementDetails } from "../../store/circuitSlice";
+import { useDispatch } from "react-redux";
 
 const ArithmeticCombinator: React.FC<EntityInterfaceProps> = ({ openSignalPicker }) => {
-  const { selectedElement } = useUIContext();
+  const { selectedElement, setSelectedElement } = useUIContext();
+  const dispacth = useDispatch();
 
   if (!selectedElement || selectedElement.name !== "arithmetic-combinator") return null;
+
+  const handleOperatorChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const operator = event.target.value;
+    dispacth(updateElementDetails({ id: selectedElement.id, details: { signals: { ...selectedElement.signals, operator } } }));
+    setSelectedElement({ ...selectedElement, signals: { ...selectedElement.signals, operator } });
+  };
 
   return (
     <div className="panel-inset-lighter mt0">
@@ -16,7 +25,13 @@ const ArithmeticCombinator: React.FC<EntityInterfaceProps> = ({ openSignalPicker
         <div className="flex-row condition-container" style={{ padding: "10px" }}>
           {RenderSignalButton({ slot: "input1", type: "input", selectedElement, openSignalPicker })}
           <div>
-            <select className="dropdown button condition-button" id="condition" defaultValue="+" onMouseDown={(e) => e.stopPropagation()}>
+            <select
+              className="dropdown button condition-button"
+              id="condition"
+              defaultValue={selectedElement.signals?.operator}
+              onMouseDown={(e) => e.stopPropagation()}
+              onChange={handleOperatorChange}
+            >
               <option value="+">{"+"}</option>
               <option value="-">{"−"}</option>
               <option value="*">{"*"}</option>
--- src/entities/interface/DeciderCombinator.tsx
+++ src/entities/interface/DeciderCombinator.tsx
@@ -3,11 +3,20 @@ import "../../App.css";
 import { EntityInterfaceProps } from "./EntityInterface";
 import { RenderSignalButton } from "../../utils/RenderSignalButton";
 import { useUIContext } from "../../context/UIContext";
+import { useDispatch } from "react-redux";
+import { updateElementDetails } from "../../store/circuitSlice";
 
 const DeciderCombinator: React.FC<EntityInterfaceProps> = ({ openSignalPicker }) => {
-  const { selectedElement } = useUIContext();
+  const { selectedElement, setSelectedElement } = useUIContext();
+  const dispacth = useDispatch();
 
   if (!selectedElement || selectedElement.name !== "decider-combinator") return null;
+
+  const handleOperatorChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const operator = event.target.value;
+    dispacth(updateElementDetails({ id: selectedElement.id, details: { signals: { ...selectedElement.signals, operator } } }));
+    setSelectedElement({ ...selectedElement, signals: { ...selectedElement.signals, operator } });
+  };
 
   return (
     <div className="panel-inset-lighter mt0">
@@ -16,7 +25,13 @@ const DeciderCombinator: React.FC<EntityInterfaceProps> = ({ openSignalPicker })
         <div className="flex-row condition-container" style={{ padding: "10px" }}>
           {RenderSignalButton({ slot: "input1", type: "input", selectedElement, openSignalPicker })}
           <div>
-            <select className="dropdown button condition-button" id="condition" defaultValue=">" onMouseDown={(e) => e.stopPropagation()}>
+            <select
+              className="dropdown button condition-button"
+              onChange={handleOperatorChange}
+              id="condition"
+              defaultValue={selectedElement.signals?.operator}
+              onMouseDown={(e) => e.stopPropagation()}
+            >
               <option value=">">{">"}</option>
               <option value="<">{"<"}</option>
               <option value=">=">{">="}</option>
